33e01ff8be90a1fbf82729052dff3961c507317e
Abhishekp2025 <abhishek@unicoconnect.com>
2025-05-19 10:43:45 +0530

    Update bullets.xml.mustache.dart

--- packages/dart_pptx/lib/src/template/ppt/slides/bullets.xml.mustache.dart
+++ packages/dart_pptx/lib/src/template/ppt/slides/bullets.xml.mustache.dart
@@ -34,7 +34,7 @@ const String template = r'''<?xml version="1.0" encoding="UTF-8"?>
                 </p:nvSpPr>
                 <p:spPr>
                      <a:xfrm>
-                        <a:off x="1000500" y="1584648"/>
+                        <a:off x="1500500" y="1384648"/>
                         <a:ext cx="8053600" cy="832614"/>
                     </a:xfrm>
                     <a:prstGeom prst="rect">
