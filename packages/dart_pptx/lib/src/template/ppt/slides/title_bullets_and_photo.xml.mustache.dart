09551d7328c62bc5293f91018dd09fa3603bcac0
Abhishekp2025 <abhishek@unicoconnect.com>
2025-05-19 11:17:21 +0530

    Update title_bullets_and_photo.xml.mustache.dart

--- packages/dart_pptx/lib/src/template/ppt/slides/title_bullets_and_photo.xml.mustache.dart
+++ packages/dart_pptx/lib/src/template/ppt/slides/title_bullets_and_photo.xml.mustache.dart
@@ -82,7 +82,7 @@ const String template = r'''<?xml version="1.0" encoding="UTF-8"?>
                 </p:blipFill>
                 <p:spPr>
                     <a:xfrm>
-                        <a:off x="10088000" y="1384648"/>
+                        <a:off x="1088000" y="1384648"/>
                         <a:ext cx="6096000" cy="3429000"/>
                     </a:xfrm>
                     <a:prstGeom prst="rect">
@@ -106,7 +106,7 @@ const String template = r'''<?xml version="1.0" encoding="UTF-8"?>
                 </p:nvSpPr>
                 <p:spPr>
                     <a:xfrm>
-                        <a:off x="10088000" y="8572500"/>
+                        <a:off x="1088000" y="8572500"/>
                         <a:ext cx="6096000" cy="342900"/>
                     </a:xfrm>
                     <a:prstGeom prst="rect">
